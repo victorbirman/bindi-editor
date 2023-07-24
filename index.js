4df8b29275eabb2c1a286e1f651e9b6740e3e057
Victor Birman <victorbirman@gmail.comn>
2023-07-24 16:26:52 -0300

    added favicon

--- index.js
+++ index.js
@@ -67,6 +67,8 @@ document.addEventListener("DOMContentLoaded", function () {
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/x-icon" href="favicon.png" />
+
         <title>${titleInput.value}</title>
         <link rel="stylesheet" href="style-articles.css" />
       </head>
